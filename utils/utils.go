0e8fc84cc4472f0a7befab6fcc7ba622596e4eea
EvanXzj <2686465170@qq.com>
2018-07-04 11:04:37 +0800

    fix runtime log path

--- utils/utils.go
+++ utils/utils.go
@@ -18,7 +18,7 @@ type CmdParams struct {
 func CmdParamsParse() CmdParams {
 	logFilePath := flag.String("logFilePath", "/tmp/logs/dig.log", "file path where to read log data to parse") // this is an address
 	routineNum := flag.Int("routineNum", 5, "consumer numble by goroutine")
-	runTimeLogPath := flag.String("runTimeLogPath", "/tmp//logs/runtime.log", "this programe runtime log target file path")
+	runTimeLogPath := flag.String("runTimeLogPath", "/tmp/logs/runtime.log", "this programe runtime log target file path")
 	flag.Parse()
 
 	// fmt.Println(flag.Parsed())
